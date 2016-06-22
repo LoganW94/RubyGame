cecc9a5a3e75482fc8a344461b83e00fbe168720
LoganW94 <loganwogoman@gmail.com>
2016-06-21 21:59:18 -0400

    forgot to save

--- startmenu.rb
+++ startmenu.rb
@@ -26,13 +26,12 @@ class StartMenu
 
 		@continue = continue
 		#add animations for option select	
-		@new_press = false
+
 		@total_time += 16.0
 		@wiggle = Math.sin(@total_time * 0.01) * 5
 
 
 		if @new_press && @window.button_down?(Gosu::KbDown)
-			@new_press = !@window.button_down?(Gosu::KbDown)
 			if @pos_y == @new_game_pos
 				@pos_y = @load_pos				
 				return @pos_y
@@ -44,7 +43,6 @@ class StartMenu
 		end
 
 		if  @new_press && @window.button_down?(Gosu::KbUp)
-			@new_press = !@window.button_down?(Gosu::KbDown)
 			if @pos_y == @quit_pos
 			   	@pos_y = @load_pos
 			   	return @pos_y
@@ -64,7 +62,7 @@ class StartMenu
         	return  $game_state = -1
         end
 
-       # @new_press = !@window.button_down?(Gosu::KbDown)
+        @new_press = !@window.button_down?(Gosu::KbDown)
 	end 
 
 
