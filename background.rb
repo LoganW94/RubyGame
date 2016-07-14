5b2c3951e860f60bbb6b5616f6edf114716914f1
LoganW94 <loganwogoman@gmail.com>
2016-07-13 21:49:21 -0400

    made some additions to player movement. still needs a few adjustments

--- background.rb
+++ background.rb
@@ -20,28 +20,77 @@ class Background
 
    	def update up, down, left, right
 
-        @up = up
-        @down = down
-        @left = left
-        @right = right
+      @up = up
+      @down = down
+      @left = left
+      @right = right
         
 
-        if @up == true
-            @pos_y += 2  
-        end
+      if @up == true && @left == true
+          @pos_y += 1
+          @pos_x += 1
+      elsif @up == true && @right == true
+          @pos_y += 1
+          @pos_x += -1
+      elsif @up == true && @down == true
+          @pos_y += 0
+          @pos_x += 0
+      elsif @up == true
+          @pos_y +=2                
+      end
 
-        if @down == true 
-            @pos_y += -2  
-        end
+      if @down == true && @left == true
+          @pos_y += -1
+          @pos_x += 1
+      elsif @down == true && @right == true
+          @pos_y += -1
+          @pos_x += -1
+      elsif @down == true && @up == true
+          @pos_y += 0
+          @pos_x += 0
+      elsif @down == true
+          @pos_y +=-2                
+      end
 
-     		if @left == true     
-     		  @pos_x += 2 
-     		end    
+      if @left == true && @up == true
+          @pos_y += 1
+          @pos_x += 1
+      elsif @left == true && @right == true
+          @pos_y += 0
+          @pos_x += 0
+      elsif @left == true && @down == true
+          @pos_y += 1
+          @pos_x += -1
+      elsif @left == true
+          @pos_x += 2                
+      end
 
-        if @right == true 
-      	    @pos_x += -2        		 
-     		end
+      if @right == true && @left == true
+          @pos_y += 0
+          @pos_x += 0
+      elsif @right == true && @up == true
+          @pos_y += 1
+          @pos_x += -1
+      elsif @right == true && @down == true
+          @pos_y += -1
+          @pos_x += -1
+      elsif @right == true
+          @pos_x +=-2                
+      end
+=begin      
 
+      if @down == true 
+          @pos_y += -2  
+      end
+
+     	if @left == true     
+     	  @pos_x += 2 
+     	end    
+
+      if @right == true 
+          @pos_x += -2        		 
+     	end
+=end
   	end
 
   	def draw
