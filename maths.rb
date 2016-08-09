54db9f354788bdfd1f593866e7edaf0f3e30edee
LoganW94 <loganwogoman@gmail.com>
2016-08-08 20:54:45 -0400

    created maths class to hold various methods that would be utilized over several classes

---
+++ maths.rb
@@ -0,0 +1,32 @@
+require 'gosu'
+
+class Maths
+
+	def colide_detect
+
+	end
+
+	def pathfinder
+
+	end
+
+	def delta_hp
+
+	end
+
+	def attack
+
+	end 
+
+	def defend
+
+	end
+
+	def enemy_spawn
+
+	end
+
+	
+
+
+end
